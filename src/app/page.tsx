b3c903ab97f21003d44f801d1adb8a21ab01c886
khoji-7 <nematxonovh@gmail.com>
2024-09-30 18:58:02 +0500

    add hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Hero from "@/components/Hero";
 import Navbar from "../components/Navbar";
 import "./globals.css"
 
@@ -6,6 +7,7 @@ export default function Home(){
     return(
         <div>
             <Navbar/>
+            <Hero/>
         </div>
     );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,16 +1,68 @@
-import React from 'react';
-import Image from 'next/image';
+"use client";
+
+import React from "react";
+import Image from "next/image";
 import cursor from "../assets/icon1.png";
-import message from "../assets/icon2.png"
-import { motion } from 'framer-motion';
-import profilepic from "../assets/profilepic.png"
+import lightning from "../assets/icon2.png";
+import {motion} from "framer-motion";
+import profilepic from "../assets/profilepic.png";
 
 const Hero = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    return (
+        <div
+            className="py-24 relative overflow-hidden"
+            style={{
+                background: "linear-gradient(to bottom, #000, #2B1942 35%, #8F5C55 60%, #DBAF6E 80%)",
+            }}
+        >
 
-export default Hero
+            <div className="absolute rounded-[50%] w-[3000px] h-[1300px] top-[400px] left-[50%] -translate-x-1/2  bg-[radial-gradient(closest-side,#000_80%,#2B1942)] " > 
+
+
+            </div>
+            <div className="relative">
+                <div className="text-8xl font-bold text-center">
+                    <h1 className="text-[#98B4Ce]">
+                        Hi, I am
+                    </h1>
+                    <h1 className="text-5xl text-[#E48A57]">
+                        Hojiakbar Nematxonov 
+                    </h1>
+                </div>
+                <motion.div
+                    className="absolute left-[280px] top-[170px]"
+                    drag
+                >
+                    <Image
+                        src={cursor}
+                        alt="cursor"
+                        width={190}
+                        height={190}
+                        draggable="false"
+                    />
+
+                </motion.div>
+                <motion.div
+                    className="absolute right-[220px] top-[20px]"
+                    drag
+                >
+                    <Image
+                        src={lightning}
+                        alt="lightning"
+                        width={120}
+                        height={120}
+                        draggable="false"
+                    />
+
+                </motion.div>
+            
+            <p className="text-center text-xl max-w-[500px] mx-auto mt-8 text-white/80">
+                I am a frontend developer focused on creating websites that provide the best experience users
+            </p>
+            <Image src={profilepic} alt="profile picture" className="h-auto w-auto mx-auto" />
+            </div>
+        </div> 
+    );
+};
+
+export default Hero;
