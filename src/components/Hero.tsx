605cd8fcc8a31702122badcf2437687149bc5f66
khoji-7 <nematxonovh@gmail.com>
2024-09-30 18:31:46 +0500

    first commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Image from 'next/image';
+import cursor from "../assets/icon1.png";
+import message from "../assets/icon2.png"
+import { motion } from 'framer-motion';
+import profilepic from "../assets/profilepic.png"
+
+const Hero = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Hero
