e88802a794dc90d016375ee2502bb46e9872b399
khoji-7 <nematxonovh@gmail.com>
2024-10-01 20:31:59 +0500

    finish Skils

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -10,7 +10,21 @@ const Skills = () => {
     ]
   return (
     <div className='bg-[linear-gradient(to_top,#000,#381A5F_80%)] py-32'>
-        
+        <div className='text-white  max-w-[1000px] mx-auto p-8 text-center'>
+            <h2 className='text-6xl font-bold mb-4'>What I Do</h2>
+            <div className='grid grid-cols-2 md:grid-cols-4 gap-6'>
+            {
+                skillIcons.map((skill, index)=>(
+                    <div 
+                    key={index}
+                    className=' h-[160px] w-[140px] md:h-[220px] md:w-[220px] flex flex-col justify-between items-center bg-white/10 p-4 rounded-xl'>
+                        {skill.icon}
+                        <p className='mt-2'>{skill.label}</p>
+                    </div>
+                ))
+            }
+            </div>
+        </div>
     </div>
   )
 }
