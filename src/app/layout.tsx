90bfc2e62d846d3d43f8ed82d2810e4bae99f59a
khoji-7 <nematxonovh@gmail.com>
2024-10-16 09:19:39 +0500

    ad li

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,22 @@
 export const metadata = {
-  title: 'Hojiakbar ',
-  description: 'Potfolio',
-}
+  title: 'Hojiakbar',
+  description: 'Portfolio',
+  icons: {
+    icon: '../assets/kh.png', 
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/svg+xml" href="../assets/kh.png" />
+      </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
