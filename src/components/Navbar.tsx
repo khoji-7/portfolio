8bf86803bcaf782da56ae866eaa3edc4161eedb2
Xojiakbar <113297358+khoji-7@users.noreply.github.com>
2025-04-11 17:25:21 +0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -38,7 +38,7 @@ const Navbar = () => {
     };
 
     return (
-        <nav className="text-white/70 pt-6 w-full">
+        <nav className="text-black/70 pt-6 w-full">
             <div className="hidden md:flex items-center px-4 py-2 mx-auto  md:max-w-[400px]">
                 <ul className="flex flex-row p-4 space-x-8">
                     {navLinks.map((link, index) => (
@@ -51,7 +51,7 @@ const Navbar = () => {
                     <li> 
                     
                         <a href="#contact" className="group">
-                            <h1 className="text-lg font-bold text-white/70 cursor-pointer">Contact Me</h1>
+                            <h1 className="text-lg font-bold text-black/70 cursor-pointer">Contact Me</h1>
                             <div className="relative">
                                 <div className="absolute w-2/3 h-1 transition-all duration-300 ease-out bg-orange-400 rounded-full group-hover:w-full"></div>
                                 <div className="absolute w-1/3 h-1 transition-all duration-300 ease-out bg-orange-600 rounded-full group-hover:w-full"></div>
@@ -61,7 +61,7 @@ const Navbar = () => {
                 </ul>
             </div>
 
-            <div onClick={toggleNav} className="md:hidden absolute top-5 right-5 border rounded text-white/70 border-white/70 p-2 z-50">
+            <div onClick={toggleNav} className="md:hidden absolute top-5 right-5 border rounded text-black/70 border-white/70 p-2 z-50">
                 {nav ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} />}
             </div>
 
